69f7f83c7d468e107bc3e6ce7135bf3dfb5344e0
BestPaladin <45981792+BestPaladin@users.noreply.github.com>
2019-01-12 12:50:02 +1100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,7 +13,7 @@ client.on("ready", () => {
   console.log("\nC:/Users/degai/Desktop/GamerBot> node index.js");
   console.log("\nPrincipaleBot est allumé !\n");
   client.user.setActivity("Bonne année !");
-  client.user.setStatus('streaming').catch console.log("Status invalide.");
+  client.user.setStatus('streaming');
 }); 
 
 var mc = ":robot: Vous n'avez pas la permission d'effectuer une commande dans les messages privés. :robot:";
